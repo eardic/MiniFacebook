ea909be4ee401bb477217958cd8c57f130bbfe7d
Emre <emreardic1@gmail.com>
2014-03-28 18:05:04 +0200

    İlk versiyon.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MiniFacebook::Application.config.secret_key_base = 'ddb754427192c4ad750ad6a5f9733868d77306760a57d1f11ab74187d9ba14a589bfd33c79076c8ffbc8b407da0faeddf48e567a2a0b7fa16003966578422185'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MiniFacebook::Application.config.session_store :cookie_store, key: '_MiniFacebook_session'
