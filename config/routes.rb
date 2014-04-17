539ba8f0f6d59307151581756155b25ee0a934d7
Emre <emreardic1@gmail.com>
2014-04-17 22:04:38 +0300

    Diyalog pencereleri eklendi. Basic info edit edilebiliyor. Settings, About ve profildeki update,edit işlemleri diyaloglar ile gerçekleştiriliyor. İçleri doldurulması gereken diyaloglar var.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ MiniFacebook::Application.routes.draw do
   get "facebook/find_friends"
   get "facebook/log_out"
   post "facebook/post_text"
+  patch "facebook/update_basic"
   get "login/new"
   post "login/login_try"
   post "login/create"
